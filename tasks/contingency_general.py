5a069e18893926deeff7755f86243031e90a0975
Jay H <mobeets@gmail.com>
2023-06-23 21:57:59 -0400

    multiple rewards and contingencies task

--- tasks/contingency_general.py
+++ tasks/contingency_general.py
@@ -13,11 +13,11 @@ device = torch.device('cpu')
 
 class ContingencyGeneral(Dataset):
     def __init__(self,
-                cue_probs=[0.5, 0.5], # prop. of trials with each cue
-                rew_sizes=[[1,0], [0,1]], # reward sizes per cue
-                cue_visible_probs=[1.0, 1.0], # prop. of trials where cue is visible
-                rew_times=[9, 9], # reward time per cue
-                rew_probs=[1.0, 1.0], # probability of reward on a given trial
+                cue_probs=[0.33, 0.33, 0.34], # prop. of trials with each cue
+                rew_sizes=[[1,0], [0,1], [0,1]], # reward sizes per cue
+                cue_shown=[True, True, False], # whether or not cue is shown
+                rew_times=[9, 9, 9], # reward time per cue
+                rew_probs=[1.0, 1.0, 1.0], # probability of reward on a given trial
                 jitter=1,
                 ntrials=1000,
                 ntrials_per_episode=20,
@@ -27,14 +27,15 @@ class ContingencyGeneral(Dataset):
                 omission_trials_have_duration=True,
                 include_null_input=False):
         self.ntrials = ntrials
-        self.cue_probs = cue_probs
+        self.cue_probs = np.array(cue_probs) / sum(cue_probs)
         self.rew_sizes = rew_sizes
-        self.cue_visible_probs = cue_visible_probs
+        self.cue_shown = cue_shown
         self.jitter = jitter
         self.rew_times = rew_times
         self.rew_probs = rew_probs
         
         self.ncues = len(self.cue_probs)
+        self.ncues_shown = sum(self.cue_shown)
         self.nrewards = len(self.rew_sizes[0]) # reward dimensionality
         self.ntrials_per_cue = np.round(np.array(self.cue_probs) * self.ntrials).astype(int)
         self.ntrials_per_episode = ntrials_per_episode
@@ -51,6 +52,8 @@ class ContingencyGeneral(Dataset):
         self.make_trials()
         if len(set([len(rs) for rs in self.rew_sizes])) != 1:
             raise Exception("rew_sizes must all have the same length (reward dimensionality)")
+        if not all(self.cue_shown) and not all([self.cue_shown[i] for i in range(self.cue_shown.index(False)-1)]):
+            raise Exception("All hidden cues must be listed last")
         if self.iti_max != 0 and self.iti_dist != 'uniform':
             raise Exception("Cannot set iti_max>0 unless iti_dist == 'uniform'")
             
@@ -66,8 +69,7 @@ class ContingencyGeneral(Dataset):
         assert isi >= 0
         
         # n.b. including input for all cues, even if they're not shown
-        cue_is_shown = np.random.rand() <= self.cue_visible_probs[cue]
-        return Trial(cue, iti, isi, rew_size, cue_is_shown, self.ncues, self.t_padding, self.include_reward, self.include_null_input)
+        return Trial(cue, iti, isi, rew_size, self.cue_shown[cue], self.ncues, self.t_padding, self.include_reward, self.include_null_input)
     
     def make_trials(self, cues=None, ITIs=None):
         if cues is None:
